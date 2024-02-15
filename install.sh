e3191d21f2f5f8ce9b95536de6790a8490e7ab97
Dane Evans <dane@2pisoftware.com>
2024-02-15 21:19:04 +1100

    fix dotfile hash

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ if [ ! -d $Workspace_Dir'/.dotfiles' ]; then
 fi
 
 echo "Dotfile loaded at $(date +"%H:%M:%SS, %d_%m_%Y")" > $LOG_FILE
-echo "dotfile commit hash: " >> $LOG_FILE
-git --git-dir='./.dotfiles/.git' rev-parse HEAD >> $LOG_FILE
+echo "dotfile commit hash: ${git rev-parse HEAD}" >> $LOG_FILE
+git rev-parse HEAD >> $LOG_FILE
 
 
 # add .dotfiles folder to the gitignore
