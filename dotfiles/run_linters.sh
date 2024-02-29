2f778197d822aecf02b7e514d42c9710024fdaf2
Dane Evans <dane@2pisoftware.com>
2024-02-29 17:33:09 +1100

    add comment

--- dotfiles/run_linters.sh
+++ dotfiles/run_linters.sh
@@ -8,13 +8,14 @@
 yamllint -s ./.github/ | tee yamllint.log
 shfmt -s -i 4 -d ./.devcontainer | tee shfmt.diff
 
-docker run \
-    -e RUN_LOCAL=true \
-    -e USE_FIND_ALGORITHM=true \
-    -e IGNORE_GITIGNORED_FILES=true \
-    -e LINTER_RULES_PATH=./.github/linters \
-    -e LOG_LEVEL=WARN \
-    -e FILTER_REGEX_INCLUDE=.devcontainer* \
-    -v ./:/tmp/lint \
-    -w /tmp/lint \
-    github/super-linter
+# havent got this one working yet. 
+# docker run \
+#     -e RUN_LOCAL=true \
+#     -e USE_FIND_ALGORITHM=true \
+#     -e IGNORE_GITIGNORED_FILES=true \
+#     -e LINTER_RULES_PATH=./.github/linters \
+#     -e LOG_LEVEL=WARN \
+#     -e FILTER_REGEX_INCLUDE=.devcontainer* \
+#     -v ./:/tmp/lint \
+#     -w /tmp/lint \
+#     github/super-linter
