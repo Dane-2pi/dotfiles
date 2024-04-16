e456cdb42a9d45fde91ccb48f543bf4287ec36bf
Dane Evans <dane@2pisoftware.com>
2024-04-16 21:11:58 +1000

    update dotfiles and linters

--- dotfiles/run_linters.sh
+++ dotfiles/run_linters.sh
@@ -4,18 +4,31 @@
 # This script will run yamllint and shfmt on the .devcontainer directory and log the output to a file.
 # It will also run the super-linter on the root directory.
 
+lint_log_dir="./lint_logs"
+mkdir -p "$lint_log_dir"
 
-yamllint -s ./.github/ | tee yamllint.log
-shfmt -s -i 4 -d ./.devcontainer | tee shfmt.diff
+# yamllint not found ... 
+yamllint -s ./.github/ | tee "$lint_log_dir/yamllint.log"
+shfmt -s -i 4 -d -w "$lint_log_dir/shfmt.diff" ./.devcontainer
 
-# havent got this one working yet. 
-# docker run \
-#     -e RUN_LOCAL=true \
-#     -e USE_FIND_ALGORITHM=true \
-#     -e IGNORE_GITIGNORED_FILES=true \
-#     -e LINTER_RULES_PATH=./.github/linters \
-#     -e LOG_LEVEL=WARN \
-#     -e FILTER_REGEX_INCLUDE=.devcontainer* \
-#     -v ./:/tmp/lint \
-#     -w /tmp/lint \
-#     github/super-linter
+
+# Run the super-linter on the root directory
+# --rm removes the container afterwards 
+docker run --rm \
+    -e LOG_LEVEL=ERROR \
+    -e RUN_LOCAL=true \
+    -e CREATE_LOG_FILE=true \
+    -e LOG_FILE=${lint_log_dir}/super-linter.log \
+    -e DEFAULT_BRANCH=main \
+    -e IGNORE_GITIGNORED_FILES=true \
+    -e VALIDATE_SHELL_SHFMT=false \
+    -e LINTER_RULES_PATH=./.github/linters \
+    -e GITHUB_ACTIONS_COMMAND_ARGS="-ignore 'SC1090'" \
+    -v ./:/tmp/lint \
+    ghcr.io/super-linter/super-linter:slim-v6.3.1 \
+   | tee ${lint_log_dir}/super-linter-summary.ans
+
+    # ghcr.io/super-linter/super-linter:latest # if some of the non-slim ones are required
+
+# chown to current user 
+sudo chown --reference README.md ${lint_log_dir}/super-linter.log
--- install_linters.sh
+++ install_linters.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-#  Install a set of linters 
+Workspace_Dir='/workspaces/'${RepositoryName}
+
+LOG_FILE=$Workspace_Dir'/.dotfiles/install_log.txt'
+
+echo "Installing linters "  >> $LOG_FILE
+
+sudo apt-get update
 
 sudo apt-get install -y yamllint
 curl -sS https://webinstall.dev/shfmt | bash
           source ~/.config/envman/PATH.env
-docker pull github/super-linter:latest 
+# docker pull github/super-linter:latest 
+
+echo "Checking if linters are installed"  >> $LOG_FILE
+yamllint --version >> $LOG_FILE
+shfmt --version >> $LOG_FILE
 
 
 #  To run  them  :
-#  cd dotfiles 
-# bash run_linters.sh
+# bash ./.dotfiles/run_linters.sh
